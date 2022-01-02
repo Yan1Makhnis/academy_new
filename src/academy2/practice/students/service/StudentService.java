3b6a820c7071978f28a11d2affc08e549ce808f8
Yan1Makhnis <ymakhnis@yandex.ru>
2022-01-02 13:50:10 +0300

    practice student added NAMES

--- src/academy2/practice/students/service/StudentService.java
+++ src/academy2/practice/students/service/StudentService.java
@@ -1,28 +1,31 @@
 package academy2.practice.students.service;
 
 import academy2.practice.students.dto.Student;
+import academy2.practice.students.dto.StudentNames;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class StudentService {
 
-    public void run(){
+    public void run() {
         List<Student> arr = new ArrayList<>();
-        for(int i = 0; i<10;i++){
+        for (int i = 0; i < 10000; i++) {
             arr.add(setRandomStudent());
         }
 
         int size = arr.size();
-        for (Student s :arr) {
+        for (Student s : arr) {
             System.out.println(s);
         }
     }
 
-    public Student setRandomStudent(){
+    public Student setRandomStudent() {
         Random r = new Random();
-        Student s = new Student("Алексей",r.nextInt(),r.nextInt(17),r.nextDouble(),r.nextBoolean());
-        return s;
+        String name = StudentNames.names[r.nextInt(100)];
+        return new Student(name, r.nextInt(), r.nextInt(7, 18), r.nextDouble(0.0, 10.01), r.nextBoolean());
+
     }
 }
